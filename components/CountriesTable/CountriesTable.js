efcf6a656a17768600ac29d6b5972adb7c71e9ea
phuochungdn1999 <phuochungdn1999@gmail.com>
2021-03-23 21:33:02 +0700

    first commit

---
+++ components/CountriesTable/CountriesTable.js
@@ -0,0 +1,85 @@
+import { KeyboardArrowDownRounded, KeyboardArrowUpRounded } from '@material-ui/icons';
+import { useState } from 'react';
+import styles from './CountriesTable.module.css';
+import Link from 'next/Link';
+
+const orderBy=(countries,value,direction)=>{
+    if(direction ==='asc'){
+        return [...countries].sort((a,b)=>a[value]>b[value]?1:-1);
+    }
+    if(direction==='desc'){
+        return [...countries].sort((a,b)=>a[value]>b[value]?-1:1);
+    }
+    return countries;
+}
+
+const SortArrow = ({direction})=>{
+    if(!direction)
+        return <></>;
+    if(direction==='desc'){
+        return (
+            <div className={styles.heading_arrow}>                    
+                <KeyboardArrowDownRounded color="inherit"/>
+            </div>
+        )
+    }
+    if(direction==='asc'){
+        return (
+            <div className={styles.heading_arrow}>                    
+                <KeyboardArrowUpRounded color="inherit"/>
+            </div>
+        )
+    }
+}
+
+const CountriesTable = ({countries}) => {
+    const [direction,setDirection] = useState();
+    const [value,setValue] = useState();
+
+    const orderCountries = orderBy(countries,value,direction);
+
+    const switchDirection = ()=>{
+        if(!direction){
+            setDirection('desc');
+        }
+        else if(direction==='desc'){
+            setDirection('asc');
+        }else{
+            setDirection(null);
+        }
+    }
+
+    const setValueAndDirection = (value)=>{
+        switchDirection();
+        setValue(value);
+    }
+
+    return (
+        <div>
+            <div className={styles.heading}>
+                <button className={styles.heading_name} 
+                onClick={()=>setValueAndDirection('name')}>
+                    <div>Name</div>
+                    <SortArrow />
+                </button>
+                <button className={styles.heading_population} 
+                onClick={()=>setValueAndDirection('population')}>
+                    <div>Population</div>
+                    <SortArrow direction={direction}/>
+                </button>
+            </div>
+            {orderCountries.map((country)=>(
+                <Link href={`/country/${country.alpha3Code}`}>
+                    <div  className={styles.row}>
+                        <div className={styles.name}>{country.name}</div>
+                        <div className={styles.population}>{country.population}</div>
+                    </div>
+                </Link>
+                
+            ))}
+        </div>
+        
+    )
+}
+
+export default CountriesTable
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,44 @@
 import Head from 'next/head'
+import { useState } from 'react'
+import CountriesTable from '../components/CountriesTable/CountriesTable'
+import Layout from '../components/Layout/Layout'
+import SearchInput from '../components/SearchInput/SearchInput'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({countries}) {
+  const [keyword,setKeyWord] = useState('')
+
+  const filteredCountries = countries.filter(country=>
+    country.name.toLowerCase().includes(keyword) ||
+    country.region.toLowerCase().includes(keyword) ||
+    country.subregion.toLowerCase().includes(keyword) 
+  );
+
+  const onInputChange=(e)=>{
+    e.preventDefault();
+    setKeyWord(e.target.value.toLowerCase())
+  }
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Layout>
+        <div className= {styles.counts}>found {countries.length} countries</div>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <SearchInput placeholder='Filter by Name, Region or SubRegion'
+          onChange={onInputChange}
+        />
+        <CountriesTable countries={filteredCountries}/>
+      </Layout>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
   )
+}
+
+export const getStaticProps = async()=>{
+  const res = await fetch('https://restcountries.eu/rest/v2/all');
+  const countries = await res.json();
+
+  return {
+    props:{
+      countries
+    }
+  }
 }
